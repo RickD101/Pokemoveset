fc54b6d05730813bcb8d60d0eb9f8bea4ee99911
RickD <ricky.pleace@gmail.com>
2020-05-27 17:23:51 +1000

    finished touching up appearance of search screen

--- js/app.js
+++ js/app.js
@@ -13,10 +13,12 @@ const domManager = {
         $('.container').children().remove();
 
         // draw the pokemon search bar and button
-        $('.container').append('<div class="row d-flex align-items-center" style="height: 100vh; margin-top: 70;"></div>');
+        $('.container').append('<div class="form-row d-flex align-content-center justify-content-center" style="height: 100vh; margin-top: 70;"></div>');
         const searchBar = $(
-            '<input class="form-control form-control-lg" id="searchBar" type="text" placeholder="Search a Pokemon by name..."></input>')
-        $('.row').append(searchBar);
+            '<div class="col-12 col-sm-6 p-1"><input class="form-control form-control-lg" id="searchBar" type="text" placeholder="Search a Pokemon by name..."></input></div>');
+        const goButton = $(`<div class="p-1"><button class="btn btn-primary btn-lg">Go!</button></div>`);
+        $('.form-row').append(searchBar);
+        $('.form-row').append(goButton);
     }
 };
 
